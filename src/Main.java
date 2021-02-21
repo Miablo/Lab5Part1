e8c2499a1922330fdeaad6a2eb30b6d01d117b14
Miablo <themio@themioshow.com>
2021-02-21 12:28:41 -0500

    added main class and lab directions

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    /*
+(5 pts) Part 1 intends to auto generate test cases for a function foo that has k integers as input arguments,
+i.e., foo(int n1, int n2, …, int nk).
+Each argument belongs to a different equivalence class, which are stored in a downloadable Eq.txt file.
+The content is shown below and may be modified to handle more inputs and equivalence classes.
+
+1, 15; 16, 30
+1, 10; 11, 20; 21, 30
+1, 5; 6, 10; 11, 15
+1, 3; 4, 6; 7, 9; 10, 12
+1, 12; 13, 24
+For this file, the nth row describes the nth input. Take the second row for example.
+There are six input arguments.
+The data 1, 10; 11, 20; 21, 30 indicates that argument n2 has three equivalence classes separated by the semi-colons.
+Develop an internal method “private int check(int val)” that returns the equivalence class the val is in.
+The result of check(val = 3) for n2 will be 1 and check(val = 25) for n2 will be 3.
+Regarding the function foo, it computes the sum of the returned values by the check function for all input arguments.
+
+Follow the contents of the Eq.txt file to automatically create test cases for Strong Normal Equivalence class testing.
+The input argument values are randomly generated. Store your prepared test cases to a test.txt file.
+Each test case comes with an expected output at the end. All values are delimited by a comma.
+In other words, Eq.txt is the input and the test.txt is the output result.
+     */
+}
